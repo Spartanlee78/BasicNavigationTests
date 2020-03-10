f7ce5a5c5251f7dab7a6790f65643d3e507e7a21
Spartanlee78 <spartanlee78@gmail.com>
2020-03-10 16:05:58 -0500

    HomeWork

--- src/test/java/com/cbt/utilities/TitleVerification3.java
+++ src/test/java/com/cbt/utilities/TitleVerification3.java
@@ -1,31 +1,30 @@
 package com.cbt.utilities;
 
-
 import org.openqa.selenium.WebDriver;
-
 
 import java.util.Arrays;
 import java.util.List;
 
-public class TitleVerfication2 {
+public class TitleVerification3 {
     public static void main(String[] args) {
 
         List<String> urls = Arrays.asList("https://lulugandgeorgia.com",
-                                        "https://wayfair.com/",
-                                        "https://walmart.com" ,
-                                        "https://westelm.com/");
+                "https://wayfair.com/",
+                "https://walmart.com" ,
+                "https://westelm.com/");
 
-        WebDriver driver = BrowserFactory.createADriver("chrome");
 
-        driver.get("https://www.google.com/");
-        BrowserUtils.wait(1000);
+
+     //   WebDriver driver = BrowserFactory.createADriver("chrome");
 
         for (String eachUrl : urls) {
 
+            WebDriver driver = BrowserFactory.createADriver("chrome");
+            driver.get(eachUrl);
+            BrowserUtils.wait(1000);
             String checked = eachUrl.substring(eachUrl.indexOf("/")+2,eachUrl.indexOf(".com")).toLowerCase();
 
-            driver.navigate().to(eachUrl);
-            BrowserUtils.wait(1000);
+
             String title = driver.getTitle().toLowerCase().replaceAll(" ","");
 
             if (title.contains(checked)){
@@ -39,11 +38,9 @@ public class TitleVerfication2 {
                 System.out.println("Website Title: " + title);
                 System.out.println();
             }
-
+            driver.close();
             BrowserUtils.wait(1000);
 
         }
-
-        driver.quit();
     }
 }
